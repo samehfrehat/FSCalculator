0754b0f80b3935778310563713140c3f2ac33c9f
sameh frehat <samehfrehat@ymail.com>
2021-09-16 21:29:46 +0300

    Calculator

---
+++ SimpleCalculator/SimpleCalculator/Program.fs
@@ -0,0 +1,46 @@
+﻿namespace SimpleCalculator.Program
+
+open System
+
+type SimpleCalculator() =     
+
+    let SplitInput (input: string) (delimitersArray: string array) : string array =
+       match input with
+       | "" -> [||]
+       | _ -> input.Split(delimitersArray, StringSplitOptions.RemoveEmptyEntries)
+
+    let GetDelimitersString (input: string) : string = 
+        match input with
+        | "" -> ""
+        | _ when input.StartsWith "//[" && input.Contains "]\n"-> input.Substring(2, input.IndexOf "]\n" - 1)
+        | _ when input.StartsWith "//" && input.Contains "\n" ->
+            Char.ToString input.[2]
+        | _ -> ""
+
+    let GetCleanInput (input: string) : string = 
+        match input with
+        | "" -> ""
+        | _ when input.StartsWith "//" && input.Contains "\n" ->
+            input.Substring(input.IndexOf "\n", (input.Length - input.IndexOf "\n"))
+        | _ -> input
+
+    member simpleCalculator.Add (input: string) : int =
+
+        let delimiterString = GetDelimitersString input
+
+        let cleanInput = GetCleanInput input
+
+        let fullDelimitersArray = Array.append (delimiterString.Split([| "[" ; "]" |], StringSplitOptions.RemoveEmptyEntries)) [| "\n" ; "," |]
+
+        let numbers = [ for number in (SplitInput cleanInput fullDelimitersArray) -> Int32.Parse number ]      
+
+        let negativeValues, positiveValues = numbers |> List.partition (fun number -> number < 0)
+
+        let limitedPositiveValues = positiveValues |> List.filter (fun number -> number <= 1000 ) 
+
+        match input with
+        | "" -> 0
+        | _ -> 
+            if negativeValues.Length > 0 then raise (new Exception(sprintf "negatives not allowed %A" negativeValues))
+            List.sum limitedPositiveValues
+                
---
+++ SimpleCalculator/SimpleCalculator/Tests.fs
@@ -0,0 +1,64 @@
+﻿namespace SimpleCalculator.Program.Tests
+
+open SimpleCalculator.Program
+open NUnit.Framework
+
+[<TestFixture>]
+type Tests() =
+    let simpleCalculator = SimpleCalculator()
+
+    [<Test>]
+    member Test.EmptyString_ShouldReturn_Zero() =
+        let sum = simpleCalculator.Add ""
+        Assert.AreEqual(0, sum)
+
+    [<TestCase("1")>]
+    member Test.OneNumber_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(1, sum)
+
+    [<TestCase("1,2")>]
+    member Test.TwoNumbers_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(1+2, sum)
+
+    [<TestCase("1,2,4,6,9,12")>]
+    member Test.UnknownAmountOfNumbers_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(1+2+4+6+9+12, sum)
+    
+    [<TestCase("1\n2,3")>]
+    member Test.InputWithNewLines_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(1+2+3, sum)
+
+    [<TestCase("//;\n11;22")>]
+    member Test.InputWithCustomDelimiter_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(11+22, sum)
+
+    [<TestCase("//;\n5;-5;5;-11")>]
+    member Test.InputWithNigativeValues_ShouldReturn_Exception input =
+        let negativeException = Assert.Throws(typeof<System.Exception>, (fun () -> simpleCalculator.Add input |> ignore))
+        let expectedMessage = "negatives not allowed [-5; -11]"
+        Assert.AreEqual(expectedMessage, negativeException.Message)
+
+    [<TestCase("//;\n11;22;2000;1000;1001")>]
+    member Test.SkipNumbersMoreThan1000_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(11+22+1000, sum)
+
+    [<TestCase("//[;;;;]\n11;;;;22;;;;2000;;;;1000;;;;1001")>]
+       member Test.InputContainLongDelimiter_ShouldReturn_Sum input =
+           let sum = simpleCalculator.Add input
+           Assert.AreEqual(11+22+1000, sum)
+
+    [<TestCase("//[;][@][(]\n5;1@2(1000")>]
+    member Test.InputContainDifferentSingleDelimiter_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(5+1+2+1000, sum)
+
+    [<TestCase("//[;;][@@][({(]\n5;;1@@2({(1000")>]
+    member Test.InputContainDifferentLongDelimiter_ShouldReturn_Sum input =
+        let sum = simpleCalculator.Add input
+        Assert.AreEqual(5+1+2+1000, sum)
